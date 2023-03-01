82d047a69fc413e2156da6ec74049c231aad824b
valenhl <valenhl.0814@gmail.com>
2023-03-01 14:21:34 -0500

    CRUD WEB Categories

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
 builder.Services.AddTransient<SeedDb>();
 
 var app = builder.Build();
